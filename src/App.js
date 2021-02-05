eaa9f90827e4692e409fdbafe4208ed0ed94b140
msr1013 <rawalsinghmanish@gmail.com>
2021-02-05 18:18:06 +0545

    movie-app

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Home from './pages/Home'
+import NavBar from './components/NavBar'
+
+function App() {
+  return (
+
+    <BrowserRouter>
+      <NavBar />
+      <div className="container-fluid">
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </div>
+
+    </BrowserRouter>
+
+  );
+}
+
+export default App;
---
+++ src/config.js
@@ -0,0 +1,4 @@
+export const API_KEY = 'cb0d71883b7798e184c4dee0877a2b6d';
+export const BASE_URL = 'https://api.themoviedb.org/3';
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/w500'
+
