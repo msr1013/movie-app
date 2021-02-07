17b8f79f09ea79636aaa035bd5da627f60f77004
msr1013 <rawalsinghmanish@gmail.com>
2021-02-07 15:00:21 +0545

    pass-movie-title-from-moviecard-to-home-component

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,12 +4,12 @@ import {
     CardTitle, CardSubtitle, Button
 } from 'reactstrap';
 
-const MovieCard = (props) => {
+const MovieCard = ({ movie }) => {
     return (
         <Card className="mt-2">
             <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
             <CardBody>
-                <CardTitle tag="h5">Card title</CardTitle>
+                <CardTitle tag="h5">{movie.title}</CardTitle>
                 <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
                 <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
                 <Button>Button</Button>
