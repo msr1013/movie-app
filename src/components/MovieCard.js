9c51529dd6aa7937c1172c210e3a8925ea060390
msr1013 <rawalsinghmanish@gmail.com>
2021-02-07 15:03:20 +0545

    vote_count-api-passed-as-props-to-home-component

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -10,7 +10,7 @@ const MovieCard = ({ movie }) => {
             <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
             <CardBody>
                 <CardTitle tag="h5">{movie.title}</CardTitle>
-                <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
+                <CardSubtitle tag="h6" className="mb-2 text-muted">Vote Count : {movie.vote_count}</CardSubtitle>
                 <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
                 <Button>Button</Button>
             </CardBody>
