233dfbecd938321dd86bdf74e50035f3e0b048e4
msr1013 <rawalsinghmanish@gmail.com>
2021-02-10 15:38:42 +0545

    'final-changes-to-deploy'

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Home from './pages/Home'
 import NavBar from './components/NavBar'
-import ApiUsed from "./pages/ApisUsed";
+
 
 function App() {
   return (
@@ -12,8 +12,8 @@ function App() {
       <NavBar />
       <div className="container-fluid">
         <Switch>
-          <Route exact path="/movie" component={Home} />
-          <Route exact path="/apiused" component={ApiUsed} />
+          <Route exact path="/" component={Home} />
+
         </Switch>
       </div>
 
